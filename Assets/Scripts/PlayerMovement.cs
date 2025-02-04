62dcb29fe93ebfe2e6921b66702ce8a998900766
fikusak22 <titera.fi.2023@skola.ssps.cz>
2025-02-04 08:42:36 +0100

    Hrac se otaci
    negr se otaci do smeru kam to ma zrovna namireno

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,7 @@
+using System.Data.Common;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -7,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
     private Vector2 moveDirection;
     private Vector2 moveInput;
+    [SerializeField]private float rotationSpeed;
     [SerializeField]private float speed;
 
     private void Awake()
@@ -14,6 +17,21 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
     private void FixedUpdate()
+    {
+       SetPlayerVelocity();
+        Rotate();
+    }
+    private void Rotate()
+    {
+        if (moveInput != Vector2.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(transform.forward, moveInput);
+            Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+            rb.MoveRotation(rotation);
+        }
+    }
+    private void SetPlayerVelocity()
     {
         rb.linearVelocity = moveInput * speed;
     }
